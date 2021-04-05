aa245fc4697894a90bb01ba33895fe89c75e9c69
rileybarry <60715866+rileybarry@users.noreply.github.com>
2021-04-05 12:29:04 -0500

    Final Change

--- forms/mobileNav/mobileNav.js
+++ forms/mobileNav/mobileNav.js
@@ -1,8 +1,21 @@
+mobileNav.onshow = function() {
+  hmbMenu.hidden = false
+}
 
-hmbMenu.onclick=function(s){
-  if (typeof(s) == "object") 
+hmbMenu.onclick = function(s) {
+  if (typeof(s) == "object") {
     return
-  /* add a switch here to take the user to the form
-     they chose. 
-  */
+  } else {
+    switch (s) {
+      case "Dessert Voting":
+        ChangeForm(dessertVoting)
+        break
+      case "Describe You":
+        ChangeForm(describeYou)
+        break
+      case "Favorite Exercises":
+        ChangeForm(favExercises)
+        break
+    }
+  }
 }
