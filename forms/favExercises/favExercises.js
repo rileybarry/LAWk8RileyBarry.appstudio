63848f6588d83858930e75e75b1cc5597ba822e0
rileybarry <60715866+rileybarry@users.noreply.github.com>
2021-04-05 11:37:24 -0500

    Select update

--- forms/favExercises/favExercises.js
+++ forms/favExercises/favExercises.js
@@ -1,10 +1,26 @@
-var exercises = ["pullups","pushups","situps","jogging","plank","curls"]
-var coreExercises = ["situps","plank"]
+var exercises = ["pullups", "pushups", "situps", "jogging", "plank", "curls"]
+var coreExercises = ["situps", "plank"]
+let combineList = coreExercises + exercises
 
-favExercises.onshow=function(){
-
+favExercises.onshow = function() {
+  selExercises.clear()
+  for (i = 0; i < combineList.length; i++) {
+    selExercises.addItem(combineList[i])
+  }
 }
 
+selExercises.onclick = function() {
+  selections.push(selExercises.value)
+}
 
+btnExercise.onclick = function() {
+  if (selections[0] == "situps" && selections[1] == "plank") {
+    console.log(`You chose situps and planks - those are the two corse exercises`)
+  } else {
+    console.log(`You did not pick the two core`)
+  }
+}
 
-
+btnNextTwo.onclick = function() {
+  ChangeForm(mobileNav)
+}
